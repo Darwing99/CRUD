848759494d4e53472a94cb6f9316a83b7e616082
Darwing Hernandez <70980484+Darwing99@users.noreply.github.com>
2021-03-26 20:53:56 -0600

    version 1.3.6

--- src/main/java/com/example/CRUD/controller/DemoController.java
+++ src/main/java/com/example/CRUD/controller/DemoController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RestController
 @RequestMapping
 public class DemoController { 
     
--- src/main/java/com/example/CRUD/controller/controlerAccion.java
+++ src/main/java/com/example/CRUD/controller/controlerAccion.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RestController
+
 @RequestMapping
 public class controlerAccion {
     @Autowired
